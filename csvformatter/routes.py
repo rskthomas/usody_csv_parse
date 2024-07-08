0e6e9971fb287f64440ad715f049e1cfcf112aae
Thomas Rusiecki <rusieckithomas@gmail.com>
2024-07-08 04:02:24 -0300

    history function added

--- csvformatter/routes.py
+++ csvformatter/routes.py
@@ -14,9 +14,27 @@ def hello_world():
     return render_template('home.html')
 
 csv_storage = {}
+VOLUME_PATH = '/app/files'
 
-@app.route('/preview', methods=['GET', 'POST'])
-def upload_file():
+
+@app.route('/preview', defaults={'file_name': None}, methods=['GET', 'POST'])
+@app.route('/preview/<file_name>', methods=['GET', 'POST'])
+def upload_file(file_name):
+
+    if file_name:
+        #open file and read the content
+
+
+        file_path = os.path.join(VOLUME_PATH, file_name)
+        #open the file and read the content using panda
+        data = pd.read_csv(file_path, sep=',', encoding='utf-8')
+        # Convert the DataFrame to a list of dictionaries
+        data = data.to_dict(orient='records')
+        data = format_csv(data)
+
+        
+        return render_template('preview.html', data=data, filename=file_name, date=datetime.datetime.now(), temp_id=None)
+       
     #if metho is post, check if file is in request.files
     if request.method == 'POST':
         if 'file' not in request.files:
@@ -34,20 +52,19 @@ def upload_file():
             filename = secure_filename(file.filename)
             
             try:
-               
                 data = pd.read_csv(file, sep=';', encoding='utf-8')
-                
                 # Convert the DataFrame to a list of dictionaries
                 data_list = data.to_dict(orient='records')
             except Exception as e:
                 flash(f"Error processing file: {str(e)}")
                 return redirect(request.url)
             
-            # Format the data
             data_list = format_csv(data_list)
             # Generate a unique ID for this CSV
             temp_id = str(uuid.uuid4())
             # Store the CSV content in the dictionary
+
+            #TODO: cleanup memory buffer after some time
             csv_storage[temp_id] = data_list
             
             return render_template('preview.html', data=data_list, filename=filename, date=datetime.datetime.now(), temp_id=temp_id)
@@ -84,22 +101,41 @@ def save_csv(tempID):
         flash("Invalid or expired link")
         return redirect(url_for('hello_world'))
     
-    # Specify the Docker volume path
-    volume_path = '/app/files'
-    # Generate a unique filename
+     # Generate a unique filename
     filename = f"summary_{tempID}_{datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.csv"
-    file_path = os.path.join(volume_path, filename)
+    file_path = os.path.join(VOLUME_PATH, filename)
     
-    # Save the CSV data to a file in the Docker volume
+    # Open the file for writing
     with open(file_path, 'w', newline='') as csvfile:
-        csvwriter = csv.writer(csvfile)
-        for row in data:
-            csvwriter.writerow(row)
-    
+        # If data is not empty, extract headers from the first dictionary
+        if data:
+            headers = data[0].keys()
+            csvwriter = csv.DictWriter(csvfile, fieldnames=headers)
+            # Write the header
+            csvwriter.writeheader()
+            # Write the data rows
+            for row in data:
+                csvwriter.writerow(row)
+        else:
+            print("No data to save.")
+
+   
     flash("File saved successfully")
     return redirect(url_for('hello_world'))
 
-
+@app.route('/past')
+def show_past_csv_files():
+    
+    # List all files in the directory
+    all_files = os.listdir(VOLUME_PATH)
+    
+    # Filter for CSV files
+    csv_files = [file for file in all_files if file.endswith('.csv')]
+    
+    
+    # Generate and return an HTML page that lists the CSV files
+    # Assuming you have a template called 'past.html' under your templates directory
+    return render_template('past.html', csv_files=csv_files)
     
 def allowed_file(filename):
     return '.' in filename and \
